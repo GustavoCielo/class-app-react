1629f7a943b606b5819686a130d0b798377f39b0
leorjr <l.grodriguesjr@gmail.com>
2021-07-07 12:13:40 -0300

    feat: input date

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import GlobalStyle from "./styles/global";
 import InputText from "./components/inputText";
 import InputSelect from "./components/inputSelect";
+import InputDate from "./components/inputDate";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <GlobalStyle />
       <InputText>teste</InputText>
       <InputSelect />
+      <InputDate />
     </>
   );
 }
