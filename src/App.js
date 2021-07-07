a94e2401e4a3b8262fa693f9075d51e4a7761e77
Michael <michael.philipe2013@gmail.com>
2021-07-07 14:13:34 -0300

    fix: cleaning app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import GlobalStyle from "./styles/global";
-import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
     </>
   );
 }
