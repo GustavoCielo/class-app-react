b843c4181e8e6868f6b9f89524060c0c80664495
leorjr <l.grodriguesjr@gmail.com>
2021-07-07 11:47:07 -0300

    feat: input select

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import GlobalStyle from "./styles/global";
 import InputText from "./components/inputText";
+import InputSelect from "./components/inputSelect";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <InputText>teste</InputText>
+      <InputSelect />
     </>
   );
 }
---
+++ src/components/inputSelect/index.jsx
@@ -0,0 +1,14 @@
+import { ContainerInput } from "./style";
+
+const InputSelect = () => {
+  return (
+    <ContainerInput>
+      <select>
+        <option value=""></option>
+        <option value="">valor</option>
+      </select>
+    </ContainerInput>
+  );
+};
+
+export default InputSelect;
