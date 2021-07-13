c68b8ddf4eef585db2f413dc67531b98bc3bc42e
Michael <michael.philipe2013@gmail.com>
2021-07-13 17:14:19 -0300

    fea: removing componets app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,5 @@
 import GlobalStyle from "./styles/global";
 
-import PageLogin from "./pages/Login";
-import Courses from "./pages/Courses";
-import FullContainer from "./components/FullContainer";
-import ScrollBar from "./components/ScrollBar";
-import ModalEditCourse from "./components/ModalEditCourse";
-import ModalEnterClass from "./components/ModalEnterClass";
-
 function App() {
   return (
     <>
