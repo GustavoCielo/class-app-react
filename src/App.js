86ad0daf17c5dc3123f8e06466335b6e89f9ff40
caiosampaio894 <caiosampaio894@gmail.com>
2021-07-13 11:30:12 -0300

    correcting styles to style on FullContainer

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import GlobalStyle from "./styles/global";
-import Header from "./components/Header";
-import ModalCreateCourse from "./components/ModalCreateCourse";
-import FormTeacher from "./components/FormTeacher";
-import ChatContainer from "./components/ChatContainer";
-import RegisterStudent from "./pages/RegisterStudent";
+
 import PageLogin from "./pages/Login";
+import Courses from "./pages/Courses";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <Header />
-      <FormTeacher />
-      <ModalCreateCourse />
-      <ChatContainer></ChatContainer>
-      <RegisterStudent />
+      <Courses />
       <PageLogin />
     </>
   );
---
+++ src/pages/Courses/index.jsx
@@ -0,0 +1,13 @@
+import FullContainer from "../../components/FullContainer";
+import Header from "../../components/Header";
+import Aside from "../../components/Aside";
+const Courses = () => {
+  return (
+    <FullContainer>
+      <Header />
+      <Aside />
+    </FullContainer>
+  );
+};
+
+export default Courses;
