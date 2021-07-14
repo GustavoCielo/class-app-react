d841a157b844482a1ccade39fec5bbb474e16182
caiosampaio894 <caiosampaio894@gmail.com>
2021-07-14 15:37:25 -0300

    doing page Courses and correct modal edit course

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
 import GlobalStyle from "./styles/global";
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-import Header from "./components/Header";
->>>>>>> 93339878985a7110088064eafad34acceb4f68f9
-=======
->>>>>>> 05f3b6599ee1d7d4d56a71f3f73a8861e4a5c68f
-
+import Courses from "./pages/Courses";
+import ModalEditCourse from "./components/ModalEditCourse";
+import ModalCreateCourse from "./components/ModalCreateCourse";
 function App() {
   return (
     <>
       <GlobalStyle />
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-      <Header />
->>>>>>> 93339878985a7110088064eafad34acceb4f68f9
-=======
->>>>>>> 05f3b6599ee1d7d4d56a71f3f73a8861e4a5c68f
+      <Courses />
+      <ModalEditCourse />
+      <ModalCreateCourse />
     </>
   );
 }
